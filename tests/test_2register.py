1cb6a2bc1282edbfd1156b101cb38e65480dfbdc
Charles Horn <charles.horn@gmail.com>
2022-09-28 01:17:02 +1300

    I think this is sufficient to build a TC proof Any algorithm must be converted into a single outer while() loop with as multiple if(v.reg) tests and as much state as needed, provided by unlimited prime-encoded virtual registers...

--- tests/test_2register.py
+++ tests/test_2register.py
@@ -182,7 +182,7 @@ def test_if_decv_code_execution(initial_state, final_state, result, path):
                 #    m.inc(1)
                 path_taken += 'B'
                 test_result = False
-                # set v.reg.2 to 0 to force a failed test for 'True' path
+                # set v.reg.2 to 1 to avoid a failed test for 'True' path
                 while(m.dec(0)):
                     m.inc(1)
                     m.inc(1)
@@ -197,7 +197,7 @@ def test_if_decv_code_execution(initial_state, final_state, result, path):
                 m.inc_by(0, 3)
             path_taken += 'B'
             test_result = False
-            # set v.reg.2 to 0 to force a failed test for 'True' path
+            # set v.reg.2 to 1 to avoid a failed test for 'True' path
 
             while(m.dec(0)):
                 m.inc(1)
@@ -227,3 +227,98 @@ def test_if_decv_code_execution(initial_state, final_state, result, path):
     assert prime_decode(m.counters[0]) == final_state
     assert test_result == result
     assert path_taken == path
+
+
+while_cases = [
+    [[0, 0, 2], [], '.'],
+    [[0, 0, 3], [], '.'],
+    [[0, 1, 2], [], 'A.'],
+    [[0, 3, 2], [], 'AAA.'],
+    [[0, 5, 3], [], 'AAAAA.'],
+    [[0, 6, 3], [], 'AAAAAA.'],
+]
+
+
+@pytest.mark.parametrize('initial_state,final_state,path', while_cases)
+def test_while_decv_code_execution(initial_state, final_state, path):
+    # This test demonstrates how 2 register PMMN can implement
+    # a single outer program loop which can be used to iterate
+    # code blocks based on the value of a virtual prime encoded
+    # register.
+
+    m = ProgramMachine(2)
+
+    # set v.reg.2, v.reg.3, and v.reg.5
+
+    r = 2**initial_state[0] * 3**initial_state[1] * 5**initial_state[2]
+    for i in range(r):
+        m.inc(0)
+    m.show()
+
+    assert prime_decode(m.counters[0]) == initial_state
+    path_taken = ''
+
+    m.dec(0)
+    while m.dec(0):  # keep performing the v.reg.while if data is in reg 0
+        m.inc(0)
+        m.inc(0)
+        # test v.reg.3, set v.reg.2 to 1 on Fail to avoid a failing test on v.reg.3 = True case
+        while m.dec(0):
+            if m.dec(0):
+                if m.dec(0):
+                    pass
+                else:
+                    # early zero, we are done
+                    print('EARLY ZERO B')
+                    path_taken += '.'
+                    # clear ALL data for HALT,
+                    # but set v.reg.2 to 1 to avoid a failed test for 'True' path
+                    while(m.dec(1)):
+                        pass
+                    m.inc(1)
+            else:
+                # early zero, we are done
+                print('EARLY ZERO A')
+                path_taken += '.'
+                # clear ALL data for HALT,
+                # but set v.reg.2 to 1 to avoid a failed test for 'True' path
+                while(m.dec(1)):
+                    pass
+                m.inc(1)
+
+            m.inc(1)
+
+        # Data is in reg 1 at this point
+
+        # Test v.reg.2 to trigger a failed test if previous v.reg test was True
+        while m.dec(1):
+            if m.dec(1):
+                pass
+            else:
+                # early zero, failed test, v.reg.2 was 0, previous v.reg test was True
+                path_taken += 'A'
+                while(m.dec(0)):  # Reset v.reg.2 to 0
+                    m.inc_by(1, 2)
+
+                while(m.dec(1)):  # Transfer data from 1 -> 0
+                    m.inc(0)
+            m.inc(0)
+
+        m.dec(0)
+
+    m.show()
+    # The only way to break out of the outer while() which contains inner while()s acting as v.reg if()s
+    # is to have both physical registers zeroed.
+    # One register must be 0 to break out of the last inner while (testing a v.reg), and the other
+    # must be zero to break out of the main program loop, indicating a HALT condition has been reached.
+    assert prime_decode(m.counters[0]) == []
+    assert prime_decode(m.counters[1]) == []
+    assert m.counters[0] == 0
+    assert m.counters[1] == 0
+    # However, we can test what path we took and the result of arbitrary terminating computations
+    # will have been present encoded in one of the data registers.
+    assert path_taken == path
+    # Non-terminating algorithms will continue to modify the data register, and the outer loop
+    # will never terminate, as expected.
+    # An alternative HALT state could be to retain the final state register value and enter an infite loop
+    # within the last failed test of a if(v.reg)
